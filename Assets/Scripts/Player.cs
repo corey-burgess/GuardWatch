e443f6e7eed0d4f2da1cc9d074b53678438e264e
Corey Burgess <coreyburgess@Coreys-MBP.lan>
2020-06-10 23:09:48 -0400

    GitHub Upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event System.Action OnReachedEndOfLevel;
+
+    public float moveSpeed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    Rigidbody rigidBody;
+    bool disabled;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if (!disabled)
+        {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Finish")
+        {
+            Disable();
+            if(OnReachedEndOfLevel != null)
+            {
+                OnReachedEndOfLevel();
+            }
+
+        }
+    }
+
+    void Disable ()
+    {
+        disabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidBody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+}
